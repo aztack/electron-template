0ff68bb9abc54497cb7edc8f8cf1edb600cd85f6
wangweihua <wangweihua@vipkid.com.cn>
2021-01-28 17:30:22 +0800

    init

---
+++ meta.js
@@ -0,0 +1,44 @@
+const $fs = require('fs');
+const $path = require('path');
+
+module.exports = {
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "label": "Project name, no space or punctuations, for example: my-project"
+    },
+    "description": {
+      "type": "string",
+      "required": true,
+      "label": "Project description",
+      "default": "An Android Project"
+    },
+    "author": {
+      "type": "string",
+      "label": "Author"
+    }
+  },
+  complete: function (data, opts) {
+    const cwd = $path.join(process.cwd(), data.inPlace ? '' : data.destDirName);
+    const name = data.name;
+
+    process.chdir(cwd);
+    const npm = require('npm');
+    const projectPkgJson = $path.join(cwd, 'package.json');
+    npm.load(projectPkgJson, (err) => {
+      if (!err) {
+        npm.install(process.cwd(), (err) => {
+          if (err) {
+            console.log(`Install package failed:`, error);
+          } else {
+            npm.commands.run(['start']);
+          }
+        });
+      } else {
+        console.error(`Load ${projectPkgJson} failed`, err);
+      }
+    });
+  },
+  skipInterpolation: []
+}
